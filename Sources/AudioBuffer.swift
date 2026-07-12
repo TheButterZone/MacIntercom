bbe00546b9f554240ea3402aea6d3689e80e8bf4
TheButterZone <thebutterzone@cox.net>
2026-07-12 09:04:28 +0000

    Add shared audio buffer

---
+++ Sources/AudioBuffer.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+final class AudioBuffer {
+
+    private var samples: [Float] = []
+    private let lock = NSLock()
+
+    func write(_ newSamples: [Float]) {
+        lock.lock()
+        samples.append(contentsOf: newSamples)
+        lock.unlock()
+    }
+
+    func read(count: Int) -> [Float] {
+        lock.lock()
+        defer {
+            lock.unlock()
+        }
+
+        let amount = min(count, samples.count)
+
+        let result = Array(samples.prefix(amount))
+
+        samples.removeFirst(amount)
+
+        return result
+    }
+
+}
