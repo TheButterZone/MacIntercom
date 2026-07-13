4216b8152d425fe71953dc8b8917b4a001fcdff1
TheButterZone <thebutterzone@cox.net>
2026-07-13 06:46:30 +0000

    Add linear interpolation resampler

--- Sources/AudioResampler.swift
+++ Sources/AudioResampler.swift
@@ -5,27 +5,45 @@ final class AudioResampler {
     init() {
     }
 
-    func process(
-	_ samples: [Float]
-    ) -> [Float] {
+func process(
+    _ samples: [Float]
+) -> [Float] {
 
-	var output: [Float] = []
-
-	output.reserveCapacity(
-	    samples.count * 12
-	)
-
-	for sample in samples {
-
-            for _ in 0..<6 {
-
-		output.append(sample)
-		output.append(sample)
-
-	    }
-	}
-
-	return output
+    guard !samples.isEmpty else {
+        return []
     }
+
+    var output: [Float] = []
+
+    output.reserveCapacity(
+        samples.count * 12
+    )
+
+    for i in 0..<(samples.count - 1) {
+
+        let a = samples[i]
+        let b = samples[i + 1]
+
+        for step in 0..<6 {
+
+            let t = Float(step) / 6.0
+
+            let sample =
+                a + (b - a) * t
+
+            output.append(sample)
+            output.append(sample)
+        }
+    }
+
+    let last = samples.last!
+
+    for _ in 0..<6 {
+        output.append(last)
+        output.append(last)
+    }
+
+    return output
+}
 
 }
